dfcb873fc416caf15368c8456c6753704429d3ee
HenriqueNasciment0 <hsncorretor@gmail.com>
2023-04-15 13:33:55 -0300

    Sessão 2, dia 2, Exercicio 1 (Antecessor e sucessor de um número)

---
+++ src/sessao2Dia2Ex1/AntecessorSucesso.java
@@ -0,0 +1,20 @@
+package sessao2Dia2Ex1;
+
+import java.util.Scanner;
+
+public class AntecessorSucesso {
+
+	public static void main(String[] args) {	
+		try(Scanner scan = new Scanner(System.in)){
+
+		System.out.println("Digite um número:");
+		int numero = scan.nextInt();
+		
+		int antecessor = numero - 1;
+		int sucessor = numero + 1;
+	
+		System.out.println("O número " + numero + " tem "+ antecessor + " como seu antecessor e " + sucessor + " como seu sucessor.");
+		}
+	}
+
+}
